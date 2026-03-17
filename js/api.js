20ce886a096c3fc983a3d74cafe152c5e5928495
Vadym Sosnitskiy <vadiksosnitskiy98@gmail.com>
2026-03-17 09:36:02 +0100

    Api fix v2.0

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 export async function getrandomQuote() {
-  const respons = await fetch("https://api.quotable.io/random");
+  const respons = await fetch("https://dummyjson.com/quotes/random");
   if (!respons.ok) {
     throw new Error("API error");
   }
@@ -7,7 +7,7 @@ export async function getrandomQuote() {
   const data = await respons.json();
 
   return {
-    text: data.content,
+    text: data.quote,
     author: data.author,
   };
 }
